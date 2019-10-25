ef28ccfc68a9d911bd405dc60712ad829713881e
Flavien <Flavien@10.11.26.21>
2019-10-25 16:29:32 +0200

    connexion

--- src/MySQL/Connexion.java
+++ src/MySQL/Connexion.java
@@ -4,16 +4,16 @@ import java.sql.*;
 public class Connexion {
 	
 	public  static Connection creeConnexion() {
-		String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/ledeux2u_RevuesOnLine?serverTimezone=Europe/Paris";
-		String login = "ledeux2u_appli";
-		String pwd = "31802662";
+		String url = "jdbc:mysql://localhost:3306/revuesonline";
+		String login = "FLedeux";
+		String pwd = "Ledeux";
 		Connection maConnexion = null;
 		try { 
 			maConnexion = (Connection) DriverManager.getConnection(url, login, pwd);
 			} 
 		catch (SQLException sqle) 
 		{
-			System.out.println("Erreur connexion" + sqle.getMessage());
+			System.out.println("Erreur connexion " + sqle.getMessage());
 			}
 		return maConnexion;
 	}
